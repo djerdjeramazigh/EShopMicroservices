f68076a477e8be973c04f1e0629bb5291cb2888e
djerdjeramazigh <djerdjeramazigh@gmail.com>
2025-07-14 21:11:02 +0200

    Products

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+
+namespace BuildingBlocks.CQRS
+{
+    internal interface ICommandHandler<in TCommand, TResponse>:IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price):IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler: IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Here you would typically interact with your database to create the product
+        // For this example, we will just return a new Guid as the product Id
+        var newProductId = Guid.NewGuid();
+        return Task.FromResult(new CreateProductResult(newProductId));
+    }
+    }
+{
+
+}
+}
